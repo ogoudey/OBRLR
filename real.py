4159ec3a8483c8b23746bc84d19cb71ff62e3d89
Olin <olin.goog@gmail.com>
2025-04-13 21:17:27 -0400

    first commit of ugly skeleton

---
+++ real.py
@@ -0,0 +1,86 @@
+from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
+from kortex_api.autogen.client_stubs.DeviceManagerClientRpc import DeviceManagerClient
+from kortex_api.autogen.client_stubs.DeviceConfigClientRpc import DeviceConfigClient
+
+from kortex_api.autogen.messages import Session_pb2, Base_pb2, Common_pb2
+
+
+
+def move_to_start_position(base):
+    # Make sure the arm is in Single Level Servoing mode
+    base_servo_mode = Base_pb2.ServoingModeInformation()
+    base_servo_mode.servoing_mode = Base_pb2.SINGLE_LEVEL_SERVOING
+    base.SetServoingMode(base_servo_mode)
+    
+    # Move arm to ready position
+    constrained_joint_angles = Base_pb2.ConstrainedJointAngles()
+
+    actuator_count = base.GetActuatorCount().count
+    angles = [0.0] * actuator_count
+
+    # Actuator 4 at 90 degrees
+    for joint_id in range(len(angles)):
+        joint_angle = constrained_joint_angles.joint_angles.joint_angles.add()
+        joint_angle.joint_identifier = joint_id
+        joint_angle.value = angles[joint_id]
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+
+    print("Reaching joint angles...")
+    base.PlayJointTrajectory(constrained_joint_angles)
+
+    print("Waiting for movement to finish ...")
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    if finished:
+        print("Joint angles reached")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+
+def move_vector(base, vector):
+    joint_speeds = Base_pb2.JointSpeeds()
+    period = 1
+    i = 0
+    for vel in vector:
+        joint_speed = joint_speeds.joint_speeds.add()
+        joint_speed.joint_identifier = i 
+        joint_speed.value = vel
+        joint_speed.duration = 0
+        i = i + 1
+    print ("Sending the joint speeds for", period, "seconds...")
+    base.SendJointSpeedsCommand(joint_speeds)
+    time.sleep(period)
+
+def rl()
+    # robosuite training
+    # => poliicy, parameters
+    # attempt irl
+
+def main()
+    # based on example 04-send_joint_speeds
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+    import utilities
+
+    # Parse arguments
+    args = utilities.parseConnectionArguments()
+
+    with utilities.DeviceConnection.createTcpConnection(args) as router:
+        print("Movement test")
+        # Create required services
+        base = BaseClient(router)
+        move_to_start_position(base)
+        
+        # ... somehow...
+        move_vector([20, 10, 0, -10, -20, 0, 0])
+        
+        rl()
+        
+
+if __name__ == "__main__":
+    main()
