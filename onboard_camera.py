c320ff9b420192fc653aff5d8a2c6b85d38a6403
Olin <olin.goog@gmail.com>
2025-04-23 18:18:03 -0400

    Add files via upload
    script for taking real pictures

--- onboard_camera.py
+++ onboard_camera.py
@@ -1,4 +1,6 @@
 import cv2
+from PIL import Image
+import random
 
 # replace ip/user/pass as needed
 rtsp_url = "rtsp://admin:admin@192.168.1.10/color"
@@ -6,6 +8,9 @@ cap = cv2.VideoCapture(rtsp_url)
 
 while True:
     ret, frame = cap.read()
+    img = Image.fromarray(frame, 'RGB')
+    img_name = 'onboard_camera'+str(random.random()*10000)+'.png'
+    img.save('real/data/onboard/' +img_name)
     if not ret:
         break
     cv2.imshow("Color Stream", frame)
