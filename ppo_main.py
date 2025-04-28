25cc4a5d649afa6a97aa2e7ddfdbc54b924d5e65
hrilab <daniel_little@verizon.net>
2025-04-28 12:12:55 -0400

    ppo files

---
+++ ppo_main.py
@@ -0,0 +1,46 @@
+
+import argparse
+import yaml
+
+import logging
+logging.disable(logging.INFO)
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+if __name__ == "__main__":
+    
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--params', type=str, required=False, help="Parameters file")
+    parser.add_argument('--pi', type=str, required=False, help="Pre-trained policy")
+    parser.add_argument('--q1', type=str, required=False, help="Pre-trained critic1")
+    parser.add_argument('--q2', type=str, required=False, help="Pre-trained critic2")
+    parser.add_argument('--rb', type=str, required=False, help="Existing replay buffer")
+    parser.add_argument('--real', action='store_true')
+    args = parser.parse_args()
+    
+    
+    # Load parameters file
+    try:
+        with open(args.params, "r") as f:
+            params = yaml.safe_load(f)
+        print("Loaded parameters")
+    except Exception:
+        print(args.params, "no good. Loading default parameters...")
+        with open("parameters/params.yaml", "r") as f:
+            params = yaml.safe_load(f)
+    
+    if args.real:
+        from real import real # irl robot stuff
+
+    import interface
+    sim = interface.Sim()
+    
+    import soft_actor_critic as sac # includes policy network
+    
+
+    sac.train(sim, params["training_parameters"], args)
+
+    sac.test(sim)
+    print("Done.")
